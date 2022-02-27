803537b3f2f654028d6c1147ee4d6856bfc48312
feynman <2901954062@qq.com>
2022-02-27 20:09:21 +0800

    初始化md-loader

--- md-loader/makedown-loader.js
+++ md-loader/makedown-loader.js
@@ -2,6 +2,13 @@
  * @Author: pink
  * @Date: 2022-02-27 19:59:03
  * @LastEditors: pink
- * @LastEditTime: 2022-02-27 19:59:04
+ * @LastEditTime: 2022-02-27 20:09:06
  * @Description: md-loader
  */
+
+module.exports = source => {
+  // 加载到的模块内容 => '#About\n\nthis is a makedown file.'
+  console.log(source)
+  // 返回值就是最终被打包内容
+  return 'hello loader'
+}
