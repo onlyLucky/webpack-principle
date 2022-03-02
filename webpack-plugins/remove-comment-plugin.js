5c3eb15494136dc402b7f03c67482806891e890a
feynman <2901954062@qq.com>
2022-03-02 21:26:00 +0800

    开发插件初始化

---
+++ webpack-plugins/remove-comment-plugin.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: pink
+ * @Date: 2022-03-02 21:21:27
+ * @LastEditors: pink
+ * @LastEditTime: 2022-03-02 21:25:36
+ * @Description: 移除注解插件
+ */
---
+++ webpack-plugins/src/main.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: pink
+ * @Date: 2022-03-02 21:05:30
+ * @LastEditors: pink
+ * @LastEditTime: 2022-03-02 21:05:30
+ * @Description: content
+ */
