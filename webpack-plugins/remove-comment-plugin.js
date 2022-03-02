da10acb036e9a777702484ea1f010c0d67f3cb86
feynman <2901954062@qq.com>
2022-03-02 23:02:56 +0800

    自定义插件文件代码正则处理

--- webpack-plugins/remove-comment-plugin.js
+++ webpack-plugins/remove-comment-plugin.js
@@ -2,7 +2,7 @@
  * @Author: pink
  * @Date: 2022-03-02 21:21:27
  * @LastEditors: pink
- * @LastEditTime: 2022-03-02 22:52:20
+ * @LastEditTime: 2022-03-02 23:01:21
  * @Description: 移除注解插件
  */
 
@@ -13,7 +13,13 @@ class RemoveCommentsPlugin{
     compiler.hooks.emit.tap('RemoveCommentsPlugin',compilation=>{
       // compilation 可以理解为此次打包的上下文
       for(const name in compilation.assets){
-        console.log(name)//输出文件名称
+        // console.log(name)//输出文件名称
+        const contents = compilation.assets[name].source()
+        const noComments = contents.replace(/\/\*{2,}\/\s?/g,'')
+        compilation.assets[name] = {
+          source: ()=>noComments,
+          size: ()=>noComments.length
+        }
       }
     })
   }
