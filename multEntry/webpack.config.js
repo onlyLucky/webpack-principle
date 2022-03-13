f5737a6c8cdefc213a04f93e02cd005e39acc59f
feynman <2901954062@qq.com>
2022-03-13 12:54:54 +0800

    多入口打包，分离公用模块

--- multEntry/webpack.config.js
+++ multEntry/webpack.config.js
@@ -2,11 +2,12 @@
  * @Author: pink
  * @Date: 2022-03-12 23:52:23
  * @LastEditors: pink
- * @LastEditTime: 2022-03-13 12:32:42
+ * @LastEditTime: 2022-03-13 12:53:24
  * @Description: webpack.config
  */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
+  mode: 'none',
   entry: {
     index: './src/index.js',
     album: './src/album.js'
@@ -33,11 +34,25 @@ module.exports = {
       }
     ]
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'//自动提取所有公共模块到单独的bundle
+    }
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Multi Entry',
+      year: new Date().getFullYear(),
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      chunks: ['index'] //指定使用index.bundle.js
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Multi Entry',
+      year: new Date().getFullYear(),
+      template: './src/album.html',
+      filename: 'album.html',
+      chunks: ['album'] //指定使用album.bundle.js
     })
   ]
 }
